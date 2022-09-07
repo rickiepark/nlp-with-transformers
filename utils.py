07512839c97ec66cb3053016973f2728660d3320
Haesun Park <haesunrpark@gmail.com>
2022-09-08 01:04:58 +0900

    10장 datasets 버전 수정

--- utils.py
+++ utils.py
@@ -32,6 +32,7 @@ def display_library_versions(libraries):
         # umap-learn은 패키지 이름과 임포트 이름이 다릅니다.
         if l == 'umap-learn':
             l = 'umap'
+        l = l.split('==')[0]    # 버전 넘버 제외
         m = importlib.import_module(l)
         # birtviz의 경우 __version__ 속성이 없으므로
         # __version__ 속성이 있는 모듈만 버전을 표시합니다.
